11c338897634788fa591f647a28f2055e214b4a2
Jianingzhang235 <jianing_chang@hotmail.com>
2019-06-21 19:00:22 -0700

    app.get('env') help you know the environments

--- express/index.js
+++ express/index.js
@@ -6,13 +6,22 @@ const logger = require('./logger');
 const authe= require('./authentica');
 // var path = require('path');
 const app = express();
+
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+// console.log(`app: ${app.get('env')}`);
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(logger);
 app.use(authe);
+if(app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled...');
+};
 app.use(helmet());
-app.use(morgan('tiny'));
+
 
   
 const courses = [
