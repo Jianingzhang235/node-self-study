4f5a50971902d3a49894b507e8604414e85a1d6d
Jianingzhang235 <jianing_chang@hotmail.com>
2019-06-26 11:14:38 -0700

    callback example 1

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,19 @@
 console.log('Before');
-setTimeout(()=> {
-    console.log('Reading a user from a database...');
-},2000)
+getUser(1, function(user) {
+    console.log('User', user);
+});
 console.log('After');
+// console.log(user);
+
+//three ways:
+  //callbacks
+  //promises
+  //async/await
+
+function getUser(id, callback) {
+    setTimeout(()=> {
+        console.log('Reading a user from a database...');
+        callback ({id: id, gitHubUsername: 'mosh'});
+    },2000);
+    
+}
