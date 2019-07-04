eca7a5cfc2a22dcb86bf9510c68c658e8acd7b28
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-04 16:18:17 -0700

    Added persistence to genres api

--- genres/index.js
+++ genres/index.js
@@ -1,13 +1,30 @@
-const Joi = require('joi');
+// const mongoose =require('mongoose');
+// const genres = require('./routes/genres');
+// const express = require('express');
+// const app = express();
+
+// mongoose.connect('mongodb://localhost/vidly',  {useNewUrlParser: true})
+//   .then(() =>console.log('Connected to MongoDB...'))
+//   .catch(err => console.error('Could not connect to MongoDB...'))
+
+// app.use(express.json());
+// app.use('api/genres', genres);
+  
+
+
+//  const port = process.env.PORT || 3006;
+//  app.listen(port, () => console.log(`Listening on port ${port}`));
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true})
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
-app.use('api/genres', genres);
-  
+app.use('/api/genres', genres);
 
-
- const port = process.env.PORT || 3000;
- app.listen(port, () => console.log(`Listening on port ${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
