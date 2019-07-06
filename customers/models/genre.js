7071ae7ef412ae0681a9dedd0fb7c50809f45f91
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-06 04:56:07 -0700

    movie model works

--- customers/models/genre.js
+++ customers/models/genre.js
@@ -1,14 +1,16 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
+const genreSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     minlength: 5,
     maxlength: 50
   }
-}));
+});
+
+const Genre = mongoose.model('Genre',  genreSchema);
 
 function validateGenre(genre) {
   const schema = {
@@ -17,6 +19,6 @@ function validateGenre(genre) {
 
   return Joi.validate(genre, schema);
 }
-
+module.exports.genreSchema = genreSchema;
 module.exports.Genre = Genre;
 module.exports.validate = validateGenre;
