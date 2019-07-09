9aeb6bff5f073124e9388f9ee7f5a7b36bb66608
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-08 23:18:13 -0700

    A Better Implementation

--- customers/models/movie.js
+++ customers/models/movie.js
@@ -31,7 +31,7 @@ const Movie = mongoose.model('Movies', new mongoose.Schema({
 function validatMovie(movie) {
   const schema = {
     title: Joi.string().min(3).required(),
-    genreId: Joi.string().required(),
+    genreId: Joi.objectId().required(),
     numberInStock: Joi.number().min(0).required(),
     dailyRentalRate : Joi.number().min(0).required(),
 };
--- customers/models/rental.js
+++ customers/models/rental.js
@@ -57,8 +57,8 @@ const Rental = mongoose.model('Rental', new mongoose.Schema({
 
 function validatRental(rental) {
   const schema = {
-    customerId: Joi.string().required(),
-    movieId: Joi.string().required()
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required()
   };
 
   return Joi.validat(rental, schema);
