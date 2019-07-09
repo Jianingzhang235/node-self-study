82b8f7e9d308f55df16066afb70023b483f10780
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-09 14:28:03 -0700

    added users router which can res to post req

--- Authentication/models/user.js
+++ Authentication/models/user.js
@@ -14,19 +14,20 @@ const User = mongoose.model('User', new mongoose.Schema({
     minlength: 5,
     maxlength: 255,
     unique: true
-  }
+  },
   password: {
     type: String,
     required: true,
     minlength: 5,
-    maxlength: 1024}
+    maxlength: 1024
+  }
 }));
 
 function validateUser(user) {
   const schema = {
     name: Joi.string().min(3).max(50).required(),
     email: Joi.string().min(3).max(255).required().email(),
-    password: Joi.string().min(3).max(255).required()
+    password: Joi.string().min(5).max(255).required()
 
   };
 
