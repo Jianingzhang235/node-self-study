444f2ece9977c0c8c79d884c1936a0d35e0fafe5
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-11 01:40:28 -0700

    add mongoose.set('useCreateIndex', true);

--- Authentication/index.js
+++ Authentication/index.js
@@ -13,6 +13,8 @@ const app = express();
 mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true})
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
+mongoose.set('useCreateIndex', true);
+
 
 app.use(express.json());
 app.use('/api/genres', genres);
@@ -20,7 +22,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
-
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3010;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
