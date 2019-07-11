f9757dd05bb27a0be9195585f804a6df19291a53
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-11 06:27:46 -0700

    auth post req works

--- vidly/index.js
+++ vidly/index.js
@@ -6,12 +6,16 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
+
 const express = require('express');
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
+mongoose.set('useCreateIndex', true);
+
 
 app.use(express.json());
 app.use('/api/genres', genres);
@@ -19,6 +23,8 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
+
 
 
 const port = process.env.PORT || 3000;
