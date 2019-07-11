4cfa11873964009bfece71702bd965fcbd205f23
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-11 03:46:34 -0700

    rewrote router users and model user

--- Authentication/routes/auth.js
+++ Authentication/routes/auth.js
@@ -12,12 +12,10 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({email: req.body.email});
   if(!user) return res.status(400).send('Invalid email or password.');
   
-  user = new User(_.pick(req.body, ['name', 'email','password']));
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-  await user.save();
-  
-  res.send(_.pick(user, ['_id','name', 'email']));
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if(!validPassword) return res.status(400).send('Invalid email or password.');
+
+  res.send(true);
 });
 
 function validateUser(req) {
@@ -26,6 +24,7 @@ function validateUser(req) {
     password: Joi.string().min(5).max(255).required()
   };
 
-  return Joi.validate(user, schema);
+  return Joi.validate(req, schema);
 }
+
 module.exports = router;
