97246067664ddec57d2af8143197b44b7f371de6
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-11 22:15:24 -0700

    Configuration property jwtPrivatekey is not defined

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
@@ -17,8 +18,7 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if(!validPassword) return res.status(400).send('Invalid email or password.');
   
-  const token = jwt.sign({_id: user._id}, 'jwtPrivatekey');
-
+  const token = jwt.sign({_id: user._id}, config.get('jwtPrivatekey'));
   res.send(token);
 });
 
