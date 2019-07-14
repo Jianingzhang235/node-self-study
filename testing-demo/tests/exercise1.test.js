d2dabd1b9345a1493e46db3bfbfa069cf8650400
Jianingzhang235 <jianing_chang@hotmail.com>
2019-07-14 04:46:33 -0700

    setted up jest-watch and passed the tests of exercies1

---
+++ testing-demo/tests/exercise1.test.js
@@ -0,0 +1,33 @@
+const exercise1 = require('../exercise1');
+
+describe('fizzBuzz', () => {
+  it('should throw an exception if input is not a number', ()  => {
+    // exercise1.fizzBuzz
+    expect(() => {exercise1.fizzBuzz('a')}).toThrow();
+    expect(() => {exercise1.fizzBuzz(null)}).toThrow();
+    expect(() => {exercise1.fizzBuzz({'a': 2})}).toThrow();
+    expect(() => {exercise1.fizzBuzz(undefined)}).toThrow();
+  });
+
+  it('should return "FizzBuzz" if input is divisible by 3 and 5', ()  => {
+    const result = exercise1.fizzBuzz(15);
+    expect(result).toBe('FizzBuzz');
+  });
+  
+  it('should return "Fizz" if input is only divisible by 3', ()  => {
+    const result = exercise1.fizzBuzz(9);
+    expect(result).toBe('Fizz');
+  });
+
+  it('should return "Buzz" if input is divisible only by 5', ()  => {
+    const result = exercise1.fizzBuzz(35);
+    expect(result).toBe('Buzz');
+  });
+
+  it('should return input if input is  not divisible by 3 or 5', ()  => {
+    const result = exercise1.fizzBuzz(7);
+    expect(result).toBe(7);
+  });
+
+
+});
